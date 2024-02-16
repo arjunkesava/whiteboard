80388c69840d8b5eccdbb108044baa68b3a78669
Arjun Kesava <arjun.kesava@gmail.com>
2024-02-16 12:18:41 +0530

    fix: stylesheet link tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
           rel="stylesheet"
-        ></link>
+        />
       </head>
       <body>
         {children}
